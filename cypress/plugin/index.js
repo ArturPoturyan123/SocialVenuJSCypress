eb4b0b09d94e827063318878cbf82aeb52398911
ArturPoturyan123 <poturyan@gmail.com>
2023-07-21 19:03:24 +0400

    testtttt

--- cypress/plugin/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = (on, config) => {
-  config.webpack.resolve.alias = {
-    ...config.webpack.resolve.alias,
-    faker: 'faker',
-  };
-  return config;
-};
---
+++ cypress/plugins/randomDataGenerator.js
@@ -0,0 +1,18 @@
+// randomDataGenerator.js
+// Import any other libraries you may need to generate random data
+const Chance = require('chance');
+
+const chance = new Chance(); // Create a new instance of Chance for generating random data
+
+// Define a function to generate random data
+const generateRandomData = () => {
+  return {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    email: chance.email(),
+    phone: chance.phone({ formatted: false, country: 'us' }), // Generate US phone numbers in non-formatted format
+  };
+};
+
+// Export the generateRandomData function to be used as 'faker' in the Cypress environment
+module.exports = generateRandomData;
