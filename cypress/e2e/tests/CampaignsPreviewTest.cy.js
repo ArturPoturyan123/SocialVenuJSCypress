a1c5055aec5272ed563d9d16e017b8f14ed43007
ArturPoturyan123 <poturyan@gmail.com>
2023-10-27 14:55:21 +0400

    added edit campaign preview test

--- cypress/e2e/tests/CampaignsPreviewTest.cy.js
+++ cypress/e2e/tests/CampaignsPreviewTest.cy.js
@@ -1,9 +1,11 @@
 import CampaignsPreviewPage from "../../pages/CampaignsPreviewPage";
 import WelcomeVideoPage from "../../pages/WelcomeVideoPage";
+import EditCampaignPage from "../../pages/EditCampaignPage";
 
 describe("Campaigns Preview Page", () => {
   const campaignsPreviewPage = new CampaignsPreviewPage();
   const welcomeVideoPage = new WelcomeVideoPage();
+  const editCampaignPage = new EditCampaignPage();
 
   beforeEach(() => {
     cy.login();
@@ -19,5 +21,11 @@ describe("Campaigns Preview Page", () => {
     campaignsPreviewPage.clickEditCampaignButton();
     campaignsPreviewPage.clickWelcomeVideoButton();
     cy.checkTextExists(welcomeVideoPage.welcomeVideoTitle);
+  });
+  it("verify the functionality to opening edit campaign page", () => {
+    campaignsPreviewPage.visit();
+    campaignsPreviewPage.clickEditCampaignButton();
+    campaignsPreviewPage.clickSmsTextMessagingButton();
+    cy.checkTextExists(editCampaignPage.editCampaignTitle);
   });
 });
--- cypress/pages/CampaignsPreviewPage.js
+++ cypress/pages/CampaignsPreviewPage.js
@@ -4,12 +4,16 @@ class CampaignsPreviewPage extends BasePage {
   returnToCampaignsButton = "#return-to-campaigns-button";
   editCampaignButton = "#composition-button";
   welcomeVideoButton = "#Welcome-Video-button";
+  smsTextMessaging = "#SMS-Text-Messaging-button";
 
   clickEditCampaignButton() {
     cy.get(this.editCampaignButton).should("be.visible").click({ force: true });
   }
   clickWelcomeVideoButton() {
     cy.get(this.welcomeVideoButton).should("be.visible").click({ force: true });
+  }
+  clickSmsTextMessagingButton() {
+    cy.get(this.smsTextMessaging).should("be.visible").click({ force: true });
   }
   getUrl() {
     return super.getUrl("reward-campaigns");
