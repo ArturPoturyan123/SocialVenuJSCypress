1d6eb7ef362043eb6e792d32175c660792403e31
arturp <poturyan@gmail.com>
2024-05-14 11:35:09 +0400

    added new reporter

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+// const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'SocialVenu Dashboard Reporter',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+      // allureWriter(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement other node event listeners here
+      // return config;
+    },
+    
+    baseUrl: "https://dashboard-release.socialvenu.com/",
+    BACKEND_URL: "https://sv-api-rc.socialvenu.com",
+    email: "arthurp@doublecoconut.com",
+    password: "123456",
+  },
+  
+});
