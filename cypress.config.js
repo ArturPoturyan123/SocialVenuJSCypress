112d3bf0d43eb5600633db6a76c0804cf4e785d6
arturp <poturyan@gmail.com>
2025-01-15 18:26:09 +0400

    added mochaswome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      // allureWriter(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+
       // implement other node event listeners here
       return config;
     },
