88826610d791b44dc9c4f66e207f04239fbb8e72
ArturPoturyan123 <poturyan@gmail.com>
2025-02-09 04:36:24 +0400

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // allureWriter(on, config);
-      require('cypress-mochawesome-reporter/plugin')(on);
-
+      allureWriter(on, config);
       // implement other node event listeners here
       return config;
     },
@@ -14,6 +11,5 @@ module.exports = defineConfig({
     BACKEND_URL: "https://sv-api-rc.socialvenu.com",
     email: "arthurp@doublecoconut.com",
     password: "123456",
-    
   },
 });
